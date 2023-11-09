0c654357a2fab73a5ccde9ac03a386c706cba7bb
amf210001 <amf210001@utdallas.edu>
2023-11-09 00:22:21 -0600

    updated UDP files for continuous and simultaneous connections

--- src/UDP/UDPClient.java
+++ src/UDP/UDPClient.java
@@ -4,32 +4,36 @@ import java.net.*;
 public class UDPClient {
     public static void main(String args[]) throws Exception 
     { 
-      BufferedReader inFromUser = 
-        new BufferedReader(new InputStreamReader(System.in)); 
-      DatagramSocket clientSocket = new DatagramSocket(); 
-  
-      InetAddress IPAddress = InetAddress.getByName("127.0.0.1"); 
-  
-      byte[] sendData = new byte[1024]; 
-      byte[] receiveData = new byte[1024]; 
-  
-      String sentence = inFromUser.readLine(); 
-      sendData = sentence.getBytes();         
+      BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+      DatagramSocket clientSocket = new DatagramSocket();
 
-      DatagramPacket sendPacket = 
-    	         new DatagramPacket(sendData, sendData.length, IPAddress, 9876); 
-    	  
-    	      clientSocket.send(sendPacket); 
-    	  
-    	      DatagramPacket receivePacket = 
-    	         new DatagramPacket(receiveData, receiveData.length); 
-    	  
-    	      clientSocket.receive(receivePacket); 
-    	  
-    	      String modifiedSentence = 
-    	          new String(receivePacket.getData()); 
-    	  
-    	      System.out.println("FROM SERVER:" + modifiedSentence); 
-    	      clientSocket.close(); 
-    	      } 
-    	}
+	  boolean connection = true;
+  
+      InetAddress IPAddress = InetAddress.getByName("localhost");
+
+	  while(connection) {
+		  byte[] sendData = new byte[1024];
+		  byte[] receiveData = new byte[1024];
+
+		  String sentence = inFromUser.readLine();
+		  if(!sentence.equalsIgnoreCase("close")) {
+			  sendData = sentence.getBytes();
+
+			  DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9877);
+
+			  clientSocket.send(sendPacket);
+
+			  DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+
+			  clientSocket.receive(receivePacket);
+
+			  String modifiedSentence = new String(receivePacket.getData());
+
+			  System.out.println("FROM SERVER:" + modifiedSentence);
+		  } else{
+			  clientSocket.close();
+			  connection = false;
+		  }
+	  }
+	}
+}
--- src/UDP/UDPServer.java
+++ src/UDP/UDPServer.java
@@ -1,37 +1,46 @@
 import java.io.*; 
 import java.net.*; 
   
-public class UDPServer {
-  public static void main(String args[]) throws Exception 
-    { 
-  
-      DatagramSocket serverSocket = new DatagramSocket(9876); 
-  
-      byte[] receiveData = new byte[1024]; 
-      byte[] sendData  = new byte[1024]; 
-  
-      while(true) 
-        { 
-  
-          DatagramPacket receivePacket = 
-             new DatagramPacket(receiveData, receiveData.length); 
-           serverSocket.receive(receivePacket); 
+public class UDPServer implements Runnable {
+    private DatagramPacket receivePacket;
+    private DatagramSocket serverSocket;
+    public UDPServer(DatagramPacket receivePacket, DatagramSocket serverSocket){
+        this.receivePacket = receivePacket;
+        this.serverSocket = serverSocket;
+    }
+    public static void main(String[] args) throws Exception {
+        try (DatagramSocket serverSocket = new DatagramSocket(9877)) {
 
-           String sentence = new String(receivePacket.getData()); 
-   
-           InetAddress IPAddress = receivePacket.getAddress(); 
-   
-           int port = receivePacket.getPort(); 
-   
-                       String capitalizedSentence = sentence.toUpperCase(); 
+            while (true) {
+                byte[] receiveData = new byte[1024];
+                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                serverSocket.receive(receivePacket);
 
-           sendData = capitalizedSentence.getBytes(); 
-   
-           DatagramPacket sendPacket = 
-              new DatagramPacket(sendData, sendData.length, IPAddress, 
-                                port); 
-   
-           serverSocket.send(sendPacket); 
-         } 
-     } 
- }  
+                UDPServer obj = new UDPServer(receivePacket, serverSocket);
+                Thread thread = new Thread(obj);
+                thread.start();
+                }
+            }
+      }
+    public void run(){
+        System.out.println("Serving client from port " + receivePacket.getPort());
+        byte[] sendData = new byte[1024];
+        String sentence = new String(receivePacket.getData());
+
+        InetAddress IPAddress = receivePacket.getAddress();
+
+        int port = receivePacket.getPort();
+
+        String capitalizedSentence = sentence.toUpperCase();
+
+        sendData = capitalizedSentence.getBytes();
+
+        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+
+        try {
+            serverSocket.send(sendPacket);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
